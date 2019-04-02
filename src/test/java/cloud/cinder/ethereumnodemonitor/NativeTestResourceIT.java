0533864028436a3ba574ea51281614d516b04f46
Quinten De Swaef <quintendeswaef@gmail.com>
2019-04-02 12:14:30 +0200

    Initial Quarkus configuration

---
+++ src/test/java/cloud/cinder/ethereumnodemonitor/NativeTestResourceIT.java
@@ -0,0 +1,9 @@
+package cloud.cinder.ethereumnodemonitor;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeTestResourceIT extends TestResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ src/test/java/cloud/cinder/ethereumnodemonitor/TestResourceTest.java
@@ -0,0 +1,21 @@
+package cloud.cinder.ethereumnodemonitor;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class TestResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/status")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
