f1d2e004d1a947d71efc7b70ac50696a39ef9633
Quinten De Swaef <quintendeswaef@gmail.com>
2019-04-02 16:37:06 +0200

    remove unused resources

--- src/test/java/cloud/cinder/ethereumnodemonitor/NativeTestResourceIT.java
+++
@@ -1,9 +0,0 @@
-package cloud.cinder.ethereumnodemonitor;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeTestResourceIT extends TestResourceTest {
-
-    // Execute the same tests but in native mode.
-}
